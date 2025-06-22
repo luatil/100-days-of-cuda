5ccdf6aac124547de0be81e4c78323a801a8fa08
Luã <luasantilli@gmail.com>
2025-06-22 17:06:58 -0300

    day_022: PrefixSum 01
    First prefix sum implementation

---
+++ day_022_prefix_sum_01.cu
@@ -0,0 +1,20 @@
+#ifndef LEET_GPU_NO_IMPORT
+#define LEET_GPU_NO_IMPORT
+#include "solve.h"
+#include <cuda_runtime.h>
+#endif
+
+__global__ void PrefixSum(const float *Input, float *Output, int N)
+{
+    Output[0] = Input[0];
+    for (int I = 1; I < N; I++)
+    {
+        Output[I] = Output[I - 1] + Input[I];
+    }
+}
+
+// input, output are device pointers
+void solve(const float *input, float *output, int N)
+{
+    PrefixSum<<<1, 1>>>(input, output, N);
+}
---
+++ day_022_prefix_sum_main.cu
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define LEET_GPU_NO_IMPORT
+
+#define Max(_a, _b) _a > _b ? _a : _b
+
+#define SOLUTION 1
+
+#if SOLUTION == 1
+#include "day_022_prefix_sum_01.cu"
+#endif
+
+int main()
+{
+    float Input[] = {1.0f, 2.0f, 3.0f, 4.0f};
+    float Expected[] = {1.0f, 3.0f, 6.0f, 10.0f};
+    float Output[sizeof(Input) / sizeof(Input[0])] = {};
+
+    float *D_Output, *D_Input;
+    cudaMalloc(&D_Input, sizeof(Input));
+    cudaMalloc(&D_Output, sizeof(Input));
+
+    cudaMemcpy(D_Input, Input, sizeof(Input), cudaMemcpyHostToDevice);
+
+    solve(D_Input, D_Output, sizeof(Input) / sizeof(Input[0]));
+
+    cudaMemcpy(Output, D_Output, sizeof(Input), cudaMemcpyDeviceToHost);
+
+    float MaxDiff = 0.0f;
+    for (unsigned int I = 0; I < (sizeof(Input) / sizeof(Input[0])); I++)
+    {
+        float Diff = abs(Output[I] - Expected[I]);
+        MaxDiff = Max(Diff, MaxDiff);
+        if (Diff > 0.00001f)
+        {
+            fprintf(stderr, "Expected [%.5f] does not match with Actual Output: [%.5f]", Expected[I], Output[I]);
+            return 1;
+        }
+    }
+    fprintf(stdout, "Maximun Difference [%.5f]", MaxDiff);
+    return 0;
+}
