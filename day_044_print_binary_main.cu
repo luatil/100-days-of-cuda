6ff345268f6730c36b106772009749030052d0b7
Luã <luasantilli@gmail.com>
2025-07-14 22:29:33 -0300

    day_044: More WIP on RadixSort
    Got correct version for first bit working now

---
+++ day_044_print_binary_main.cu
@@ -0,0 +1,25 @@
+/*
+ * NAME
+ * 	pbin - print sequence of numbers in binary format
+ *
+ * SYNOPSIS
+ * 	pbin
+ *
+ * DESCRIPTION
+ * 	Reads unsigned integer numbers from stdin and prints them in
+ * 	binary format e.g. 1010101 format.
+ *
+ * USAGE
+ * 	- Print the first 10 numbers in binary format
+ * 	seq 1 10 | pbin
+ */
+#include <stdio.h>
+
+int main()
+{
+    unsigned int Num;
+    while (scanf("%u\n", &Num) != EOF)
+    {
+        printf("%04b\n", Num);
+    }
+}
