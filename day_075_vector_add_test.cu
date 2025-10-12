b4b21685f8ad27f1a3c104c0e40c35da6ffa19a7
Lua Nowacki Scavacini Santilli <luasantilli@gmail.com>
2025-10-12 14:08:38 -0300

    wrong-commit

--- day_075_vector_add_test.cu
+++ day_075_vector_add_test.cu
@@ -12,7 +12,7 @@ __global__ void VectorAdd(float *A, float *B, float *C, int N)
   int Tid = blockDim.x * blockIdx.x + threadIdx.x;
   if (Tid < N)
   {
-    C[Tid] = A[Tid] + B[Tid];
+    C[Tid] = A[Tid] + 2.0 * B[Tid];
   }
 }
 
