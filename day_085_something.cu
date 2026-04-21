0756eb48a3fea84af6d58aaa5bb238cf44035809
Lua Nowacki Scavacini Santilli <luasantilli@gmail.com>
2026-04-21 18:13:08 -0300

    day_085: A different version of adding

---
+++ day_085_something.cu
@@ -0,0 +1,61 @@
+#include <cmath>
+#include <stdio.h>
+
+__global__ void Add(float *A, float *B, float *C, int N)
+{
+    int Tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (Tid)
+    {
+        C[Tid] = A[Tid] + B[Tid];
+    }
+}
+
+__global__ void Set(float *V, float X, int N)
+{
+    int Tid = blockDim.x * blockIdx.x + threadIdx.x;
+    if (Tid)
+    {
+        V[Tid] = X;
+    }
+}
+
+__global__ void MaxDifference(float *A, float *B, float *MaxDiff, int N)
+{
+    // NOTE(luatil): Dumb implementation here
+    float Res = -1.0f;
+    for (int I = 0; I < N; I++)
+    {
+        float Diff = abs(A[I] - B[I]);
+        Res = max(Diff, Res);
+    }
+    *MaxDiff = Res;
+}
+
+int main(int argc, char *argv[])
+{
+    int N = 1024 * 1024; // 1 MB
+    if (argc == 2)
+    {
+        N = atoi(argv[1]);
+    }
+
+    float *A, *B, *C, *Exp, *MaxDiff;
+    cudaMalloc(&A, sizeof(float) * N);
+    cudaMalloc(&B, sizeof(float) * N);
+    cudaMalloc(&C, sizeof(float) * N);
+    cudaMalloc(&Exp, sizeof(float) * N);
+    cudaMalloc(&MaxDiff, sizeof(float) * 1);
+
+    const int ThreadsPerBlock = 256;
+    const int NumOfBlocks = (N + ThreadsPerBlock - 1) / ThreadsPerBlock;
+    Set<<<NumOfBlocks, ThreadsPerBlock>>>(A, 1.0f, N);
+    Set<<<NumOfBlocks, ThreadsPerBlock>>>(B, 3.0f, N);
+    Set<<<NumOfBlocks, ThreadsPerBlock>>>(Exp, 4.0f, N);
+    Add<<<NumOfBlocks, ThreadsPerBlock>>>(A, B, C, N);
+    MaxDifference<<<1, 1>>>(C, Exp, MaxDiff, N);
+
+    float HostResult = 10.0f;
+    cudaMemcpy(&HostResult, MaxDiff, sizeof(float) * 1, cudaMemcpyDeviceToHost);
+
+    printf("MaxDiff = %f\n", HostResult);
+}
